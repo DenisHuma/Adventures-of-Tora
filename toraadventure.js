f99e92ac575714a84ca9095856bc33438b05347d
Denis10 <humadenis2001@gmail.com>
2022-09-16 22:56:20 +0300

    wip3

--- toraadventure.js
+++ toraadventure.js
@@ -1,3 +1,42 @@
+//OOP
+
+//functions
+function firstpage(){
+    frontpage.style.display = 'none';
+    let grass = document.createElement('div');
+    grass.classList.add("grass");
+    maindiv.appendChild(grass);
+    let sky = document.createElement("div");
+    sky.classList.add('sky');
+    maindiv.appendChild(sky);
+    let Tora = document.createElement('img');
+    Tora.classList.add('Tora');
+    Tora.src = "imgsrc/tora1.png";
+    sky.appendChild(Tora);
+    let chatbox = document.createElement('div');
+    chatbox.classList.add('chatbox1');
+    grass.appendChild(chatbox);
+    let textchat1 = document.createElement('p');
+    textchat1.classList.add('chat1');
+    chatbox.appendChild(textchat1);
+    textchat1.innerHTML = 'Hello, there, I am Tora! I hope you are ready to be a part of my adventures. We are going to play some games in order to get back home to my parents. They will put their fate in your skills. Whenever you are ready, press MEOW. ';
+    let meow = document.createElement('button');
+    meow.classList.add('meowbutton');
+    chatbox.appendChild(meow);
+    meow.innerHTML = 'MEOW';
+    meow.addEventListener('click', function(){
+        meow.style.display = 'none';
+        Tora.style.bottom = '-150%';
+        chatbox.style.bottom = '-100%';
+        setTimeout(function(){chatbox.style.display = 'none'; Tora.style.display = 'none';}, 2000);
+        sky.appendChild(canvas);
+    })
+}
+
+//main
+
+let canvas = document.createElement('canvas');
+canvas.classList.add('canvaspacman');
 
 window.onload = function () {
     let title = document.querySelector("#title");
@@ -5,44 +44,10 @@ window.onload = function () {
     let startbutton = document.querySelector("#startbutton");
     let maindiv = document.querySelector("#maindiv");
     startbutton.addEventListener("click", function () {
-        frontpage.style.display = 'none';
-        let grass = document.createElement('div');
-        grass.classList.add("grass");
-        maindiv.appendChild(grass);
-        let sky = document.createElement("div");
-        sky.classList.add('sky');
-        maindiv.appendChild(sky);
-        let Tora = document.createElement("div");
-        Tora.classList.add('Tora');
-        sky.appendChild(Tora);
-        let Torahead = document.createElement('div');
-        Torahead.classList.add('Torahead');
-        Tora.appendChild(Torahead);
-        let Toraeye1 = document.createElement('div');
-        Toraeye1.classList.add('eye1');
-        Tora.appendChild(Toraeye1);
-        let Toraeye2 = document.createElement('div');
-        Toraeye2.classList.add('eye2');
-        Tora.appendChild(Toraeye2);
-        let Toranose = document.createElement('div');
-        Toranose.classList.add('toranose');
-        Tora.appendChild(Toranose);
-        let leftear = document.createElement('div');
-        leftear.classList.add('leftear');
-        Tora.appendChild(leftear);
-        let rightear = document.createElement('div');
-        rightear.classList.add('rightear');
-        Tora.appendChild(rightear);
-        let tail = document.createElement('div');
-        tail.classList.add('tail');
-        Tora.appendChild(tail);
-        let chatbox = document.createElement('div');
-        chatbox.classList.add('chatbox1');
-        Tora.appendChild(chatbox);
-        let textchat1 = document.createElement('p');
-        textchat1.classList.add('chat1');
-        chatbox.appendChild(textchat1);
-        textchat1.innerHTML = 'Hello, there, I am Tora! Welcome to my adventures.. Please press MEOW'
+        firstpage();
     })
+    
+
+    
     
 }
